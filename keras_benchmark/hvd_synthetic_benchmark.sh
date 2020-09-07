e75db6e1d8fc9a1b02ac74bfffa2b5b1cb0503fa
Henrique Mendonça <henrique.mendonca@cscs.ch>
2020-09-07 15:10:50 +0200

    sbatch scripts

---
+++ keras_benchmark/hvd_synthetic_benchmark.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+ 
+#SBATCH --job-name=tfHorovodTest
+#SBATCH --time=00:15:00
+#SBATCH --switches=1
+#SBATCH --nodes=4
+#SBATCH --output=tfHorovodTest004.out
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=24
+#SBATCH --constraint=gpu
+#SBATCH --partition=normal
+
+conda activate /apps/daint/UES/6.0.UP04/sandboxes/sarafael/miniconda-tf2.3
+srun which python
+
+export NCCL_DEBUG=INFO
+export NCCL_IB_HCA=ipogif0
+export NCCL_IB_CUDA_SUPPORT=1
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+srun python -u hvd_synthetic_benchmark.py --model ResNet101 --batch-size 64
